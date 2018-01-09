a77b823bc65ad4278755f409ad2b072f12ad2905
thomasrr <thomasrr@princeton.edu>
2018-01-09 01:15:33 -0500

    3

--- analyze.py
+++ analyze.py
@@ -66,6 +66,9 @@ class location_server():
                     node = s.getsockname()[1]
                     try:
                         j = json.loads(line)
+                    except:
+                        print "excepted"
+                        continue
                     device_dict[j['packet']['AdvA']['addr']][node] = j['rssi']
                     print device_dict
 
