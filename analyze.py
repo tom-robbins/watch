4663c6793675a3c14b0184a12605d4a2b82d5c6d
thomasrr <thomasrr@princeton.edu>
2018-01-08 22:10:58 -0500

    w

--- analyze.py
+++ analyze.py
@@ -39,9 +39,9 @@ class location_server():
         return math.pow(10, (-40 - rssi)/(10*n))
 
     def start_listeners(self):
-        self.northeast = Popen("nc -l 8000 | sed 's/^/ne: /'", shell=True)
-        self.southeast = Popen("nc -l 8001 | sed 's/^/se: /'", shell=True)
-        self.southwest = Popen("nc -l 8002 | sed 's/^/sw: /'", shell=True)
+        self.northeast = Popen("nc -l 8000 | sed 's/^/ne: /'", stdout=PIPE, shell=True)
+        self.southeast = Popen("nc -l 8001 | sed 's/^/se: /'", stdout=PIPE, shell=True)
+        self.southwest = Popen("nc -l 8002 | sed 's/^/sw: /'", stdout=PIPE, shell=True)
 
     def keep_track(self):
         self.start_listeners()
